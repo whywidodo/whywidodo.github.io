cb28784e38f8d9a06e54903dc7945c27bdb2b10e
whywidodo <mr.wahyuwidodo@gmail.com>
2020-11-21 20:59:50 +0700

    init

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Karyane.com Merupakan Website Portfolio Untuk Dokumentasi Karya" />
+    <meta name="author" content="Wahyu Widodo" />
+    <title>Karyane.com - Berkarya Untuk Indonesia</title>
+    <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico" />
+    <script src="./assets/js/fontawesome-all.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <link href="./assets/css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Karyane.com</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="./assets/img/founder.jpg" alt="" /></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#tentang">Tentang</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#pengalaman">Pengalaman</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#pendidikan">Pendidikan</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#keahlian">Keahlian</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#prestasi">Prestasi</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+        <!-- Tentang -->
+        <section class="resume-section" id="tentang">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Wahyu
+                    <span class="text-primary">Widodo</span>
+                </h1>
+                <div class="subheading mb-5">
+                    +62 857-8585-5555 -
+                    <a href="mailto:mr.wahyuwidodo@gmail.com">mr.wahyuwidodo@gmail.com</a>
+                </div>
+                <p class="lead mb-5">Karyane.com merupakan sebuah website portfolio yang diciptakan sebagai media dokumentasi karya yang pernah dibuat.</p>
+                <div class="social-icons">
+                    <a class="social-icon" href="https://linkedin.com/in/wahyuwidodo"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="social-icon" href="https://github.com/whywidodo"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="https://instagram.com/mr_wahyuwidodo"><i class="fab fa-instagram"></i></a>
+                    <a class="social-icon" href="https://facebook.com/mr.wahyuwidodo"><i class="fab fa-facebook-f"></i></a>
+                    <a class="social-icon" href="https://www.youtube.com/channel/UC0SUwKCrLSn5hZJfuFz3KpQ"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Pengalaman -->
+        <section class="resume-section" id="pengalaman">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Pengalaman</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Operator Produksi</h3>
+                        <div class="subheading mb-3">PT. Aisin Indonesia Automotive</div>
+                        <p>Memproduksi komponen mobil (Oil Pan), serta memastikan bahwa barang tersebut tidak ada cacat dalam proses produksi melalui pengecekan visual.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">September 2017 - Oktober 2019</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Operator Produksi</h3>
+                        <div class="subheading mb-3">PT. Yamaha Indonesia Motor Manufacturing</div>
+                        <p>Melakukan pengecatan komponen motor (Wheel), serta memastikan bahwa barang tersebut tidak mengalami kebocoran cat di area yang tidak seharusnya terkena cat.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Maret 2016 - Maret 2017</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Administrator</h3>
+                        <div class="subheading mb-3">PT. Honda Prospect Motor</div>
+                        <p>Melakukan akurasi/pengukuran terhadap sample mobil, yang kemudian dilakukan pengecekan apakah hasil pengukuran tersebut sesuai standar atau tidak.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Oktober 2015 - Februari 2016</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Pendidikan -->
+        <section class="resume-section" id="pendidikan">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Pendidikan</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Universitas Amikom Purwokerto</h3>
+                        <div class="subheading mb-3">Teknik Informatika</div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Oktober 2020 - Sekarang</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SMK Negeri 1 Purbalingga</h3>
+                        <div class="subheading mb-3">Rekayasa Perangkat Lunak</div>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">2012 - 2015</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Keahlian -->
+        <section class="resume-section" id="keahlian">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Skil Keahlian</h2>
+                <div class="subheading mb-3">Bahasa Pemrograman & Tool</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-free-code-camp"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Prestasi-->
+        <section class="resume-section" id="prestasi">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Prestasi & Sertifikat</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Sertifikat Kompetensi OACS Graphic Design 2020 - LSK TIK
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Sertifikat Kompetensi Konsep HTML untuk Programming - Skill Academy
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Juara 2 Lomba PBB Hari Pendidikan Angkatan Laut 2013
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Portfolio-->
+        <section class="resume-section" id="portfolio">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Portfolio</h2>
+                <p>Coming Soon <i class="far fa-smile-beam"></i> </p>
+            </div>
+        </section>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="./assets/js/jquery-3.5.1.min.js"></script>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="./assets/js/jquery-1.4.1.easing.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="./assets/js/scripts.js"></script>
+</body>
+
+</html>
